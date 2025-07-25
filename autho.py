cf31f0fc4789b9b82a83e843ee2cef643122c874
HuzaifaGithub123 <huzaifadeutsch@gmail.com>
2025-07-26 00:50:57 +0200

    autho.py

--- autho.py
+++ autho.py
@@ -1,14 +1,3 @@
-import gspread
-from google.oauth2.service_account import Credentials
-import streamlit as st
-
-@st.cache_resource
-def get_sheet(sheet_name="Testing"):
-    scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
-    creds = Credentials.from_service_account_info(st.secrets["gspread"], scopes=scope)
-    client = gspread.authorize(creds)
-    return client.open(sheet_name).sheet1
-
 import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
