94541692d837cad4ffad25fcddaadaaf97dfbf38
HuzaifaGithub123 <huzaifadeutsch@gmail.com>
2025-07-26 01:49:02 +0200

    daily_sales.py

--- daily_sales.py
+++ daily_sales.py
@@ -1,22 +1,3 @@
-"""
-import streamlit as st
-from autho import get_sheet
-sheets = get_sheet()
-
-def run():
-
-    with st.form("Daily Sales"):
-        Drug_ID = st.text_input("Drug ID")
-        Drug_name = st.text_input("Drug Name")
-        Drug_Quantity = st.text_input("Enter Quantity")
-
-        data = [Drug_ID, Drug_name, Drug_Quantity]
-
-        submit = st.form_submit_button("Enter")
-        if submit:
-            sheets.append_row(data)
-            st.success("Successfully Data Entered")
-"""
 import streamlit as st
 from autho import get_sheet
 
@@ -41,5 +22,3 @@ def run():
                 st.rerun()  # This refreshes the form
             except Exception as e:
                 st.error(f"Error: {e}")
-
-run()
