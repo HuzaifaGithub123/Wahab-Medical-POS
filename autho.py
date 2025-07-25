3e9f35af74d82fbe24c37e729844f0083b746836
HuzaifaGithub123 <huzaifadeutsch@gmail.com>
2025-07-26 01:06:13 +0200

    autho.py

--- autho.py
+++ autho.py
@@ -1,3 +1,4 @@
+"""
 import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
@@ -7,6 +8,20 @@ st.title("Google Sheets Connection Test")
 
 # Auth function
 def get_sheet(sheet_name="Testing"):
+    scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
+    creds = Credentials.from_service_account_info(st.secrets["gspread"], scopes=scope)
+    client = gspread.authorize(creds)
+    sheet = client.open(sheet_name).sheet1
+    return sheet
+"""
+
+import streamlit as st
+import gspread
+from google.oauth2.service_account import Credentials
+
+@st.cache_resource
+def get_sheet(sheet_name="Testing"):
+    st.write("🔗 Connecting to Google Sheets...")  # This will show only once
     scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
     creds = Credentials.from_service_account_info(st.secrets["gspread"], scopes=scope)
     client = gspread.authorize(creds)
